c57554d3b6fe89dd632dffc8b9f69b52c9b0c829
Muhammad Rabiul Alam <bm.rabiulalam@gmail.com>
2021-02-26 23:37:57 +0600

    Rest countries data fetched, saved to state, country names list rendered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => {
+      setCountries(data);
+      console.log(data);
+      const name = data.map(country => country.name);
+      console.log(name);
+    })
+    .catch(error => console.log(error));
+  }, []);
   return (
     <div className="App">
+      <h1>Country Loaded: {countries.length}</h1>
+      <ul>
+        {
+          countries.map(country => <li>{country.name}</li>)
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
